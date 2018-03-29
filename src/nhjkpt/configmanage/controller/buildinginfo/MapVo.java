8eab18095cea47220393e71527158840003c4882
Administrator <Administrator@192.168.0.106>
2018-03-29 21:07:23 +0800

    1

---
+++ src/nhjkpt/configmanage/controller/buildinginfo/MapVo.java
@@ -0,0 +1,25 @@
+package nhjkpt.configmanage.controller.buildinginfo;
+
+public class MapVo {
+	private String id;
+	
+	private String mapAddr;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getMapAddr() {
+		return mapAddr;
+	}
+
+	public void setMapAddr(String mapAddr) {
+		this.mapAddr = mapAddr;
+	}
+	
+	
+}
